33ff74714ed7eeed8b05c289b4c87871be28ec58
unknown <dannielchristensen1@gmail.com>
2019-11-21 15:24:56 -0600

    WooHoo some ajax stuff??
    lol had to stop with the stuff cuz OnPost stuff

--- Movies/wwwroot/js/site.js
+++ Movies/wwwroot/js/site.js
@@ -2,3 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var form = document.getElementsById("search-form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var token;
+    var inputs = document.getElementsByName("input");
+    var formData = new formData();
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type == "checkbox") {
+            if (inputs[i].checked) {
+                formData.append(inputs[i].name, inputs[i].value);
+            }
+
+        } else {
+            formData.set(inputs[i].name, inputs[i].value);
+        }
+
+        if (inputs[i].name == "_RequestVerificationToken") {
+            token = inputs[i].value;
+        }
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/', true);
+    xhr.setRequestHeader("RequestVerificationToken", token);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            console.log(xhr.responseText);
+        }
+    }
+
+    xhr.send(formData);
+})
